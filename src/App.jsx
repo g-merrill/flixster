802debb4b2411d2a8a1da51bd422fa9d5ddddf7c
Greg <gmerrill03@gmail.com>
2025-05-09 00:21:05 -0700

    greet the world

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import { useState } from 'react'
 import './App.css'
 
-const App = () => {
+const App = () => (
   <div className="App">
-    
+    hello world
   </div>
-}
+)
 
 export default App
